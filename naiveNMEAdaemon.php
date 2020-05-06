4cd8db61f132a4afef4012d01c81be7276353a42
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2020-05-06 03:14:19 +0300

    NMEA fountain

---
+++ naiveNMEAdaemon.php
@@ -0,0 +1,51 @@
+<?php
+/* This is the simplest web daemon to broadcast NMEA sentences from the given file.
+Designed for debugging applications that use gpsd.
+The file set in $nmeaFileName and must content correct sentences, one per line.
+Run:
+$ php naiveNMEAdaemon.php
+gpsd run:
+$ gpsd -N -b -n tcp://192.168.10.10:2222
+*/
+$nmeaFileName = 'sample1.log'; 	// NMEA sentences file name
+//$bindAddres = "tcp://127.0.0.1:2222"; 	// Daemon's access address
+$bindAddres = "tcp://192.168.10.10:2222"; 	// Daemon's access address
+
+$run = 1800; 		// Overall time of work, in seconds. If 0 - infinity.
+$delay = 500000; 	// Min interval between sends sentences, in microseconds
+
+$strLen = 0;
+$r = array(" | "," / "," - "," \ ");
+$i = 0;
+$startAllTime = time();
+
+$socket = stream_socket_server($bindAddres, $errno, $errstr);
+if (!$socket) {
+  return "$errstr ($errno)\n";
+} 
+while ($conn = stream_socket_accept($socket)) {
+	while(!($run AND ((time()-$startAllTime)>$run))) {
+		$handle = fopen($nmeaFileName, "r");
+		if (FALSE === $handle) {
+			exit("Failed to open stream ");
+		}
+		while (!feof($handle)) {
+			$startTime = microtime(TRUE);
+			
+			$nmeaData = fread($handle, 8192);
+			//echo "$nmeaData\n";
+			fwrite($conn, $nmeaData . "\n");
+			echo($r[$i]);
+			echo " " . ($endTime-$startTime) . "                    \r";
+			$i++;
+			if($i>=count($r)) $i = 0;
+			usleep($delay);
+		};
+		fclose($handle);
+	}
+	fclose($conn);
+};
+fclose($socket);
+
+
+?>
