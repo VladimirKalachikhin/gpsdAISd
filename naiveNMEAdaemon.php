6521b4a81a29606b7b93d5100dd6f81a00f98102
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2020-05-07 18:19:42 +0300

    impr & bug fixed

--- naiveNMEAdaemon.php
+++ naiveNMEAdaemon.php
@@ -2,22 +2,27 @@
 /* This is the simplest web daemon to broadcast NMEA sentences from the given file.
 Designed for debugging applications that use gpsd.
 The file set in $nmeaFileName and must content correct sentences, one per line.
+Required options:
+-i log file name
+-b bind to proto://address:port
 Run:
-$ php naiveNMEAdaemon.php
-gpsd run:
+$ php naiveNMEAdaemon.php -isample1.log -btcp://127.0.0.1:2222
+gpsd run to connect this:
 $ gpsd -N tcp://192.168.10.10:2222
 */
-$nmeaFileName = 'sample1.log'; 	// NMEA sentences file name
-//$bindAddres = "tcp://127.0.0.1:2222"; 	// Daemon's access address
-$bindAddres = "tcp://192.168.10.10:2222"; 	// Daemon's access address
+$options = getopt("i::b::");
+if(!($nmeaFileName = filter_var(@$options['i'],FILTER_SANITIZE_URL))) $nmeaFileName = 'sample1.log'; 	// NMEA sentences file name;
+if(!($bindAddres=filter_var(@$options['b'],FILTER_VALIDATE_DOMAIN))) $bindAddres = "tcp://127.0.0.1:2222"; 	// Daemon's access address;
 
-$run = 1800; 		// Overall time of work, in seconds. If 0 - infinity.
+//$run = 1800; 		// Overall time of work, in seconds. If 0 - infinity.
+$run = 0; 		// 
 $delay = 200000; 	// Min interval between sends sentences, in microseconds. 200000 are semi-realtime for sample1.log
 
 $strLen = 0;
 $r = array(" | "," / "," - "," \ ");
 $i = 0;
 $startAllTime = time();
+$statCollection = array();
 
 $socket = stream_socket_server($bindAddres, $errno, $errstr);
 if (!$socket) {
@@ -31,7 +36,12 @@ while ($conn) { 	//
 	if (FALSE === $handle) {
 		exit("Failed to open file $nmeaFileName\n");
 	}
-	echo "Begin $nmeaFileName with delay {$delay}ms per string, send $nStr str\n";
+	if($nStr) {
+		echo "Send $nStr str                         \n";
+		statShow();
+	}
+	echo "Begin $nmeaFileName with delay {$delay}ms per string\n";
+	echo "\n";
 	$nStr = 0; 	// number of sending string
 	while (!feof($handle)) {
 		if(($run AND ((time()-$startAllTime)>$run))) {
@@ -41,6 +51,7 @@ while ($conn) { 	//
 		}
 		$startTime = microtime(TRUE);
 		$nmeaData = fgets($handle, 2048);
+		statCollect($nmeaData);
 		//echo "$nmeaData\n";
 		$res = fwrite($conn, $nmeaData . "\n");
 		if($res===FALSE) {
@@ -56,10 +67,54 @@ while ($conn) { 	//
 		usleep($delay);
 	};
 	fclose($handle);
-	if(!$conn)	$conn = stream_socket_accept($socket);
+	// reset connection?
+	//if(!$conn)	$conn = stream_socket_accept($socket);
+	fclose($conn);
+	$conn = stream_socket_accept($socket);
 }
 fclose($conn);
 fclose($socket);
 
+function statCollect($nmeaData) {
+/**/
+global $statCollection;
+$nmeaData = substr(trim(explode(',',$nmeaData)[0]),-3);
+if($nmeaData) $statCollection["$nmeaData"]++;
+/*
+if(strpos($nmeaData,'ALM')!==FALSE) $statCollection['ALM']++;
+elseif(strpos($nmeaData,'AIVDM')!==FALSE) $statCollection['AIVDM']++;
+elseif(strpos($nmeaData,'AIVDO')!==FALSE) $statCollection['AIVDO']++;
+elseif(strpos($nmeaData,'DBK')!==FALSE) $statCollection['DBK']++;
+elseif(strpos($nmeaData,'DBS')!==FALSE) $statCollection['DBS']++;
+elseif(strpos($nmeaData,'DBT')!==FALSE) $statCollection['DBT']++;
+elseif(strpos($nmeaData,'DPT')!==FALSE) $statCollection['DPT']++;
+elseif(strpos($nmeaData,'GGA')!==FALSE) $statCollection['GGA']++;
+elseif(strpos($nmeaData,'GLL')!==FALSE) $statCollection['GLL']++;
+elseif(strpos($nmeaData,'GNS')!==FALSE) $statCollection['GNS']++;
+elseif(strpos($nmeaData,'GSV')!==FALSE) $statCollection['GSV']++;
+elseif(strpos($nmeaData,'HDG')!==FALSE) $statCollection['HDG']++;
+elseif(strpos($nmeaData,'HDM')!==FALSE) $statCollection['HDM']++;
+elseif(strpos($nmeaData,'HDT')!==FALSE) $statCollection['HDT']++;
+elseif(strpos($nmeaData,'MTW')!==FALSE) $statCollection['MTW']++;
+elseif(strpos($nmeaData,'MWV')!==FALSE) $statCollection['MWV']++;
+elseif(strpos($nmeaData,'RMA')!==FALSE) $statCollection['RMA']++;
+elseif(strpos($nmeaData,'RMB')!==FALSE) $statCollection['RMB']++;
+elseif(strpos($nmeaData,'RMC')!==FALSE) $statCollection['RMC']++;
+elseif(strpos($nmeaData,'VHW')!==FALSE) $statCollection['VHW']++;
+elseif(strpos($nmeaData,'VWR')!==FALSE) $statCollection['VWR']++;
+elseif(strpos($nmeaData,'ZDA')!==FALSE) $statCollection['ZDA']++;
+elseif(strpos($nmeaData,'PGRMZ')!==FALSE) $statCollection['PGRMZ']++;
+elseif($nmeaData) $statCollection['other']++;
+*/
+} 	// end function statCollect
 
+function statShow() {
+/**/
+global $statCollection;
+ksort($statCollection);
+foreach($statCollection as $code => $count){
+	echo "$code: $count\n";
+}
+$statCollection = array();
+} // end statShow
 ?>
